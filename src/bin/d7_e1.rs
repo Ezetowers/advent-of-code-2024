acd34f99a7f89a0872b2ca8456d1d2f72b14158b
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-12-10 10:46:52 -0300

    D7 - E1 finished

--- src/bin/d7_e1.rs
+++ src/bin/d7_e1.rs
@@ -4,26 +4,71 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let _log2 = log2::stdout().module(false).level("trace").start();
+    let _log2 = log2::stdout().module(false).level("info").start();
 
-    let file = File::open("./input/d7-minimal.txt")?;
+    let file = File::open("./input/d7.txt")?;
     let reader = BufReader::new(file);
-    let mut total = 0;
+    let mut total: u64 = 0;
+    let mut line_number = 0;
 
     for line in reader.lines() {
         let line = line?;
         let split_line: Vec<&str> = line.split(":").collect();
-        let test_result = split_line[0].parse::<i32>().unwrap_or(0);
-        let test_values: Vec<i32> = split_line[1]
+        let test_result = split_line[0].parse::<u64>().unwrap_or(0);
+        let test_values: Vec<u64> = split_line[1]
             .trim()
             .split(" ")
-            .map(|x| x.parse::<i32>().unwrap_or(0))
+            .map(|x| x.parse::<u64>().unwrap_or(0))
             .collect();
+
+        let permutations = usize::pow(2, (test_values.len() - 1).try_into().unwrap());
         trace!(
-            "Test Result: {} - Test Values: {:?}",
+            "Test Result: {} - Test Values: {:?} - Permutations: {}",
             test_result,
-            test_values
+            test_values,
+            permutations,
         );
+
+        let mut valid_line = false;
+        // NOTE: Since we only have two operations, the amount of permutations that we can have
+        // can be thought as the a decomposition of a 2 power number, where every bit defines
+        // which operation needs to be made
+        // E.g. if we have 3 test_values and two operations (+,*), then there are 2^(len-1) permutations
+        // that can be made
+        // Permutations:
+        //  * 0: 0 0
+        //  * 1: 0 1
+        //  * 2: 1 0
+        //  * 3: 1 1
+        // If we replace 0 with + and 1 with *, then you have the problem solved
+        for permutation in 0..permutations {
+            let mut permutation_result: u64 = test_values[0];
+            for j in 0..test_values.len() - 1 {
+                let operation = permutation / usize::pow(2, j.try_into().unwrap()) % 2;
+                if operation == 1 {
+                    permutation_result = permutation_result * test_values[j + 1];
+                } else {
+                    permutation_result = permutation_result + test_values[j + 1];
+                }
+                trace!(
+                    "Permutation: {} - Operation: {} - Partial result: {}",
+                    permutation,
+                    operation,
+                    permutation_result
+                );
+            }
+
+            if test_result == permutation_result {
+                total += permutation_result;
+                valid_line = true;
+                break;
+            }
+        }
+        debug!(
+            "Line number: {} - Valid line: {} - Partial total: {} - Line: {}",
+            line_number, valid_line, total, line
+        );
+        line_number += 1;
     }
 
     info!("Day 7 - Exercise 1. Result: {}", total);
