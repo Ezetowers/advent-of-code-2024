ac9de57cd09078efd4901c13b295840228398cab
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-12-12 00:40:25 -0300

    D10 - E1

--- src/bin/d10_e1.rs
+++ src/bin/d10_e1.rs
@@ -1,4 +1,5 @@
 use log::*;
+use std::collections::HashSet;
 use std::error::Error;
 use std::io::{BufRead, BufReader};
 
@@ -10,23 +11,24 @@ fn trailhike_score(
     point: (i8, i8),
     previous_point: (i8, i8),
     expected_slope: u8,
-) -> u32 {
+    results: &mut HashSet<(i8, i8)>,
+) {
     // NOTE: We asume the map is square
     let map_limit = input.len();
     if point.0 < 0 || point.0 == map_limit as i8 || point.1 < 0 || point.1 == map_limit as i8 {
-        return 0;
+        return;
     }
-
     let current_slope = input[point.0 as usize][point.1 as usize];
 
     if current_slope != expected_slope {
-        return 0;
+        return;
     }
     if current_slope == 9 {
-        return 1;
+        debug!("Trailhike found. Finish point: {:?}", point);
+        results.insert(point);
+        return;
     }
 
-    let mut score: u32 = 0;
     for i in -1..=1 {
         if i == 0 {
             continue;
@@ -38,7 +40,13 @@ fn trailhike_score(
                 (point.0, point.1 + i),
                 current_slope,
             );
-            score += trailhike_score(input, (point.0, point.1 + i), point, current_slope + 1);
+            trailhike_score(
+                input,
+                (point.0, point.1 + i),
+                point,
+                current_slope + 1,
+                results,
+            );
         }
         if previous_point.0 != point.0 + i || previous_point.1 != point.1 {
             trace!(
@@ -47,10 +55,15 @@ fn trailhike_score(
                 (point.0 + i, point.1),
                 current_slope,
             );
-            score += trailhike_score(input, (point.0 + i, point.1), point, current_slope + 1);
+            trailhike_score(
+                input,
+                (point.0 + i, point.1),
+                point,
+                current_slope + 1,
+                results,
+            );
         }
     }
-    score
 }
 /*---------------------------------------------------------------------------*/
 
@@ -85,18 +98,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Trailhike starting points
-    trace!("Starting trailhike points");
+    debug!("Starting trailhike points");
     for point in trailhike_start.iter() {
-        let score = trailhike_score(
+        let mut results: HashSet<(i8, i8)> = HashSet::new();
+        trailhike_score(
             &input,
             (point.0 as i8, point.1 as i8),
             (point.0 as i8, point.1 as i8),
             0,
+            &mut results,
         );
-        trace!("Point: {:?} - Score: {}", point, score);
-        total += score;
+        debug!("Point: {:?} - Score: {}", point, results.len());
+        total += results.len();
     }
 
-    info!("Day X - Exercise Y. Result: {}", total);
+    info!("Day 10 - Exercise 1. Result: {}", total);
     Ok(())
 }
