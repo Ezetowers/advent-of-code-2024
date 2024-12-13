3af12789254178b58405198d021ac41f37a8e7a6
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-12-12 22:02:21 -0300

    D11 - E2. Fully working, and fast

--- src/bin/d11_e2.rs
+++ src/bin/d11_e2.rs
@@ -1,35 +1,22 @@
 use log::*;
-use std::collections::VecDeque;
+use std::collections::HashMap;
 use std::error::Error;
 use std::io::{BufRead, BufReader};
-use std::thread;
-use std::thread::JoinHandle;
 
 use advent_of_code_2024::common;
 
 /*---------------------------------------------------------------------------*/
 
-#[derive(Debug, Default, Copy, Clone)]
-struct Stone {
-    value: u64,
-    blink: usize,
-}
-
-/*---------------------------------------------------------------------------*/
-
-fn blink(stone: Stone) -> Vec<Stone> {
-    let mut after_blink: Vec<Stone> = Vec::new();
+fn blink(stone: u64) -> Vec<u64> {
+    let mut after_blink: Vec<u64> = Vec::new();
     // Rule 1: If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-    if stone.value == 0 {
-        after_blink.push(Stone {
-            value: 1,
-            blink: stone.blink + 1,
-        });
+    if stone == 0 {
+        after_blink.push(1);
         return after_blink;
     }
 
     // Rule 2: If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-    let string_number = stone.value.to_string();
+    let string_number = stone.to_string();
     if string_number.len() % 2 == 0 {
         let first_number_string = &string_number[0..=string_number.len() / 2 - 1];
         let first_number = first_number_string.to_string().parse::<u64>().unwrap_or(0);
@@ -43,44 +30,14 @@ fn blink(stone: Stone) -> Vec<Stone> {
                 second_number_string,
                 second_number,
             );
-        after_blink.push(Stone {
-            value: first_number,
-            blink: stone.blink + 1,
-        });
-        after_blink.push(Stone {
-            value: second_number,
-            blink: stone.blink + 1,
-        });
+        after_blink.push(first_number);
+        after_blink.push(second_number);
         return after_blink;
     }
 
     // Default: If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-    after_blink.push(Stone {
-        value: stone.value * 2024 as u64,
-        blink: stone.blink + 1,
-    });
-
+    after_blink.push(stone * 2024 as u64);
     after_blink
-}
-
-/*---------------------------------------------------------------------------*/
-
-fn process_stone(stone: Stone) -> u64 {
-    let mut stones: VecDeque<Stone> = VecDeque::new();
-    stones.push_back(stone);
-
-    let mut total = 0;
-    while stones.len() > 0 {
-        let stone = stones.pop_front().unwrap();
-        if stone.blink != 75 {
-            for new_stone in blink(stone) {
-                stones.push_front(new_stone);
-            }
-        } else {
-            total += 1;
-        }
-    }
-    total
 }
 
 /*---------------------------------------------------------------------------*/
@@ -101,38 +58,41 @@ fn main() -> Result<(), Box<dyn Error>> {
         break;
     }
 
-    // Convert the input in stones
-    let mut handles: Vec<JoinHandle<u64>> = Vec::new();
+    // Input debugging
+    trace!("Input: {:?}", input);
+    let mut previous_stones: HashMap<u64, u64> = HashMap::new();
     for i in 0..input.len() {
-        let stone_number = input[i];
-        let handle = thread::spawn(move || {
-            let stone = Stone {
-                value: stone_number,
-                blink: 0,
-            };
-            info!("[Thread {}] Processing initial stone {:#?}", i, stone);
-            process_stone(stone)
-        });
-        handles.push(handle);
+        previous_stones
+            .entry(input[i])
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
     }
 
-    let mut total = 0;
-    let mut counter = 0;
-    for handle in handles.into_iter() {
-        info!("Waiting thread {} to finish", counter);
-        match handle.join() {
-            Ok(subtotal) => {
-                total += subtotal;
-                info!(
-                    "Thread {} finished. Subtotal: {} - Total: {}",
-                    counter, subtotal, total
-                );
+    for i in 1..=75 {
+        let mut current_stones: HashMap<u64, u64> = HashMap::new();
+        for (k, v) in previous_stones.iter() {
+            let stones = blink(*k);
+            for stone in 0..stones.len() {
+                current_stones
+                    .entry(stones[stone])
+                    .and_modify(|count| *count += v)
+                    .or_insert(*v);
             }
-            Err(e) => panic!("{:#?}", e),
         }
-        counter += 1;
+        previous_stones = current_stones;
+
+        let mut partial_stones = 0;
+        for (_, v) in previous_stones.iter() {
+            partial_stones += v;
+        }
+        debug!("[BLINK {}] Partial Stones: {:?}", i, partial_stones);
     }
 
-    info!("Day 11 - Exercise 2. Result: {}", total);
+    let mut total_stones = 0;
+    for (_k, v) in previous_stones.iter() {
+        total_stones += v;
+    }
+
+    info!("Day 11 - Exercise 1. Result: {}", total_stones);
     Ok(())
 }
