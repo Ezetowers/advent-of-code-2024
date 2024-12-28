50960f76b7b1d4820df8a721a4f64c19bf2d8d6b
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-12-28 19:33:32 -0300

    D17 - E1

--- src/bin/d17_e1.rs
+++ src/bin/d17_e1.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 use advent_of_code_2024::common;
 
+#[derive(Debug)]
 struct Computer {
     reg_a: i32,
     reg_b: i32,
@@ -20,8 +21,67 @@ impl Computer {
     }
 
     fn execute_program(&mut self, program: &Vec<i32>) -> Vec<i32> {
-        let result: Vec<i32> = Vec::new();
-        result
+        let mut output: Vec<i32> = Vec::new();
+
+        let mut ip = 0;
+        loop {
+            // NOTE: Assume we get an even number of elements in the program
+            if program.len() == ip {
+                break;
+            }
+            let opcode = program[ip];
+            let operand = program[ip + 1];
+
+            debug!("Opcode: {} - Operand: {}", opcode, operand);
+            match opcode {
+                0 => {
+                    self.reg_a = self.reg_a / i32::pow(2, self.combo_operand(operand) as u32);
+                }
+                1 => {
+                    self.reg_b = self.reg_b ^ operand;
+                }
+                2 => {
+                    self.reg_b = self.combo_operand(operand) % 8;
+                }
+                3 => {
+                    if self.reg_a != 0 {
+                        ip = operand as usize;
+                    }
+                }
+                4 => {
+                    self.reg_b = self.reg_b ^ self.reg_c;
+                }
+                5 => {
+                    output.push(self.combo_operand(operand) % 8);
+                }
+                6 => {
+                    self.reg_b = self.reg_a / i32::pow(2, self.combo_operand(operand) as u32);
+                }
+                7 => {
+                    self.reg_c = self.reg_a / i32::pow(2, self.combo_operand(operand) as u32);
+                }
+                _ => panic!("This should not happen"),
+            }
+
+            if opcode == 3 && self.reg_a != 0 {
+                continue;
+            } else {
+                ip += 2;
+            }
+            debug!("Computer: {:?} - Current output: {:?}", self, output);
+        }
+        output
+    }
+
+    fn combo_operand(&self, operand: i32) -> i32 {
+        match operand {
+            0 | 1 | 2 | 3 => operand,
+            4 => self.reg_a,
+            5 => self.reg_b,
+            6 => self.reg_c,
+            7 => panic!("This should not happen"),
+            _ => panic!("This should not happen"),
+        }
     }
 }
 
@@ -54,15 +114,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let program_string = lines_it.next().unwrap()?.split(':').collect::<Vec<_>>()[1].to_string();
     let program: Vec<i32> = program_string
+        .trim()
         .split(',')
         .map(|x| x.parse::<i32>().unwrap_or(0))
         .collect();
 
     let mut computer = Computer::new(reg_a, reg_b, reg_c);
-    let result = computer.execute_program(&program);
-
     trace!("Program. String: {} - Array: {:?}", program_string, program);
 
-    info!("Day X - Exercise Y. Result: {:?}", result);
+    let result = computer.execute_program(&program);
+    info!(
+        "Day X - Exercise Y. Computer: {:#?} - Result: {:?}",
+        computer, result
+    );
     Ok(())
 }
